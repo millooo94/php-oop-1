4ed063deca82c1b088539599aba2dcd9f3957192
millooo94 <camillomusmeciunict@gmail.com>
2023-01-10 09:19:22 +0100

    add all

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ class Movie {
 }
 
 
-
 $apocalypseNow = new Movie('Apocalypse Now', 'Apocalypse Now', 'English', 5);
 
 $apocalypseNow->overview = "A Saigon, durante la guerra in Vietnam il capitano dei corpi speciali Benjamin Willard riceve dai superiori l'ordine di trovare ed eliminare il colonnello Walter Kurtz che uscito con i suoi soldati dai ranghi dell'esercito americano, sta combattendo una guerra personale ai confini fra il Vietnam e la Cambogia. Scortato da alcuni uomini, Willard risale un fiume a caccia del colonnello. Dopo varie peripezie il capitano individua Kurtz all'interno di una sorta di reggia-tempio protetta dalla vegetazione e da numerosi indigeni armati. Willard non sa che fare, ma è lo stesso Kurtz che lo induce ad eseguire la condanna.";
@@ -46,17 +45,6 @@ $jaws = new Movie('Jaws', 'Lo squalo', 'English', 4);
 $jaws->overview = "Ad Amity, cittadina balneare del New England, si sparge il terrore per gli attacchi di un terribile squalo bianco. Un poliziotto, un ittiologo e un cacciatore di squali si mettono in mare per dargli la caccia.";
 
 var_dump($jaws);
-
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
